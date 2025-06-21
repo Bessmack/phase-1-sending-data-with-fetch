be4df2e18b0b2fc9f5d37949fc08274e6d94e391
Bessmack <jenwied4lif@gmail.com>
2025-06-21 17:59:05 +0300

    Sending Data with Fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const toSubmit = {
+        name: userName,
+        email: userEmail
+    };
+
+    const postUser = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(toSubmit)
+    };
+
+    return fetch("http://localhost:3000/users", postUser)
+        .then(res => {
+            return res.json();
+        })
+        .then(userInfo => {
+            const span = document.createElement('span');
+            span.textContent = userInfo.id;
+            document.body.appendChild(span);
+            console.log(userInfo);
+        })
+        .catch(error => {
+            const p = document.createElement('p');
+            p.textContent = error.message;
+            document.body.appendChild(p);
+            console.error(error);
+        });
+}
+submitData("Brian Okuku", "brayo@gmail.com");
